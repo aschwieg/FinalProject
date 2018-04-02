b79a2c358dda684a53e6827bb1e743e7e2b4eeb1
Alex Schwiegeraht <aschwieg@gmail.com>
2018-04-02 18:03:06 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Alex's Homepage</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style.css">
+</head> 
+<body>
+<header>
+	<h1>The Final Project</h1>
+</header>
+<nav>
+	<a href="">Link</a> <a href="">Link</a> <a href="">Link</a> <a href="">Link</a>
+</nav>
+<main>
+	<h2>Main</h2>
+</main>
+<footer>
+	<h2>Footer</h2>
+</footer>
+</body>
+</html>
